2465d7198b84e10c659818296881b329f6f63358
tommyhetterscheid17 <tommyhetterscheid@gmail.com>
2017-05-04 18:01:32 +1000

    add integration test users_login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -4,4 +4,16 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+ test 'login with invalid information' do 
+ 	get login_path
+ 	assert_template 'sessions/new'
+ 	post login_path, params: {session: {email: "", password: ""}}
+ 	assert_not flash.empty?
+ 	get root_path
+ 	assert flash.empty?
+ end
+
 end
+
+
