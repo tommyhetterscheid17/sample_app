b6f3a3accfa9e27d3b9f22c292eef1217ba19532
tommyhetterscheid17 <tommyhetterscheid@gmail.com>
2017-05-05 17:06:15 +1000

    implement basic = log-in:

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -31,6 +31,24 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   	assert is_logged_in?
   end
 
+  test "login with valid information followed by logout" do
+    get login_path
+    post login_path, params: { session: { email:    @user.email,
+                                          password: 'password' } }
+    assert is_logged_in?
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+    delete logout_path
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path,      count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
+  end
 end
+
 
 
